bd79ddf56bb4b9994b1eb9304948d3ec5fbb3f86
FM <ge57sak@mytum.de>
2022-11-19 12:48:07 +0100

    First stuff

---
+++ SampleReader.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+std::string get_sample_text_from_file()
+{
+
+}
--- TypingTrainer.cpp
+++ TypingTrainer.cpp
@@ -1,1 +1,39 @@
 #include <iostream>
+#include <ncurses.h>
+
+int main(int argc, char ** argv)
+{
+    initscr();
+
+    WINDOW *sample_text_win = newwin(3, 100, 0, 0);
+    WINDOW *input_text_win = newwin(3, 100, 3, 0);
+    WINDOW *statistic_info_win = newwin(3, 100, 6, 0);
+    refresh();
+
+    box(sample_text_win, 0, 0);
+    box(input_text_win, 0, 0);
+    box(statistic_info_win, 0, 0);
+
+    mvwprintw(sample_text_win, 0, 1, "Text to type");
+    mvwprintw(sample_text_win, 1, 1, "This is the sample text");
+    mvwprintw(input_text_win, 0, 1, "Your input");
+    mvwprintw(input_text_win, 1, 1, "Here, we will show the input");
+    mvwprintw(statistic_info_win, 0, 1, "Statistics");
+    mvwprintw(statistic_info_win, 1, 1, "Here, we will show the statistics");
+
+    wrefresh(sample_text_win);
+    wrefresh(input_text_win);
+    wrefresh(statistic_info_win);
+
+    noecho()
+    char c{};
+
+    while((c = getch()) != 'a')
+    {
+        mvwprintw(input_text_win, 1, 1, &c);
+        wrefresh(input_text_win);
+    }
+
+    endwin();
+    return 0;
+}
