26c215f2d7585dd3a53e8729f1d4619ae8fa6f39
FM <ge57sak@mytum.de>
2022-11-19 22:27:48 +0100

    First working basic prototype

--- TypingTrainer.cpp
+++ TypingTrainer.cpp
@@ -24,48 +24,112 @@ void show_start_menu();
 */
 string init_typing_trainer(int difficulty);
 
+bool is_valid_text_char(char c)
+{
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') 
+    || c == '.' || c == ',' || c == ' ';
+}
+
 /*
     This function runs the typing trainer.
 */
-void run_typing_trainer(string & sample_text);
+void run_typing_trainer(const string sample_text)
+{
+    initscr();
+
+    // Here, we define how wide the output window is
+    int window_length = 100;
+
+    // We initialize and enable the color capability of the terminal
+    start_color();
+    init_pair(2, COLOR_WHITE, COLOR_RED);
+
+    // We define the windows and set up some graphical properties
+    WINDOW *sample_text_win = newwin(3, window_length, 0, 0);
+    WINDOW *input_text_win = newwin(3, window_length, 3, 0);
+    WINDOW *statistic_info_win = newwin(3, window_length, 6, 0);
+    box(sample_text_win, 0, 0);
+    box(input_text_win, 0, 0);
+    box(statistic_info_win, 0, 0);
+    mvwprintw(sample_text_win, 0, 1, "Text to type");
+    mvwprintw(input_text_win, 0, 1, "Your input");
+    mvwprintw(statistic_info_win, 0, 1, "Statistics");
+    mvwprintw(statistic_info_win, 1, 1, "Here, we will show the statistics");
+    refresh();
+    wrefresh(sample_text_win);
+    wrefresh(input_text_win);
+    wrefresh(statistic_info_win);
+
+    // noecho ensures that user typing is not automatically written into the terminal
+    // (because we want to do that manually)
+    noecho();
+
+    // User's current typing position in text. E.g. position 4 for "Foo Bar" means 
+    // that the user needs to input the character 'B' now
+    int pos_in_sample_text = 0;
+    
+    // Number of characters in the sample text
+    int len_of_sample_text = sample_text.size();
+
+    // How many characters we can display in a window (-2 because of the window borders)
+    int displayable_text_len = window_length - 2;
+
+    // The position of the first character that is currently being shown in the window
+    // E.g. sample text: "ABCDEFGHIKL" -> If only "CDEFGHIJKL" can be shown in the window
+    // (because more cannot fit), the variable takes value 2.
+    int text_segment_start = 0;
+
+    // The character that has last been input
+    char input_char;
+
+    // We run the typing trainer until we are at the end of the sample text
+    while(text_segment_start < len_of_sample_text)
+    {
+        // Compute which part of the string is currently displayed in the window
+        string current_text_segment = sample_text.substr(text_segment_start, displayable_text_len);
+
+        // Display the text to be shown
+        mvwprintw(sample_text_win, 1, 1, current_text_segment.c_str());
+        wrefresh(sample_text_win);
+
+        // Refresh the shown text in the input window
+        mvwprintw(input_text_win, 1, 1, (current_text_segment.substr(0, pos_in_sample_text - text_segment_start)).c_str());
+        wrefresh(input_text_win);
+
+        // Read the user input from input, store it in input_char and check if the input was correct.
+        // If it was incorrect, repeat until it's correct
+        while((input_char = getch()) != sample_text[pos_in_sample_text])
+        {
+            // Input was incorrect: Display a red box around the wrongly input character
+            if(is_valid_text_char(input_char))
+            {
+                mvwprintw(input_text_win, 1, 1, current_text_segment.substr(0, pos_in_sample_text - text_segment_start).c_str());
+                wattron(input_text_win, COLOR_PAIR(2));
+                mvwaddch(input_text_win, 1, pos_in_sample_text - text_segment_start + 1, input_char);
+                wattroff(input_text_win,COLOR_PAIR(2));
+                wrefresh(input_text_win);
+            }
+        }
+
+        // This is a cosmetic fix that is needed
+        mvwaddch(input_text_win, 1, pos_in_sample_text - text_segment_start + 1, ' '); 
+
+        // When we finally input the correct char, we mve on to the next
+        pos_in_sample_text++;
+
+        // In the beginning, the current input position will move from left to right.
+        // At half of the window size, we stop moving the input position and move
+        // the whole text instead
+        if(pos_in_sample_text > displayable_text_len / 2)
+            text_segment_start++;
+    }    
+
+    endwin();
+}
 
 
 
 int main(int argc, char ** argv)
 {
-    initscr();
-
-    WINDOW *sample_text_win = newwin(3, 100, 0, 0);
-    WINDOW *input_text_win = newwin(3, 100, 3, 0);
-    WINDOW *statistic_info_win = newwin(3, 100, 6, 0);
-    refresh();
-
-    box(sample_text_win, 0, 0);
-    box(input_text_win, 0, 0);
-    box(statistic_info_win, 0, 0);
-
-    mvwprintw(sample_text_win, 0, 1, "Text to type");
-    mvwprintw(sample_text_win, 1, 1, "This is the sample text");
-    mvwprintw(input_text_win, 0, 1, "Your input");
-    mvwprintw(statistic_info_win, 0, 1, "Statistics");
-    mvwprintw(statistic_info_win, 1, 1, "Here, we will show the statistics");
-
-    wrefresh(sample_text_win);
-    wrefresh(input_text_win);
-    wrefresh(statistic_info_win);
-
-    noecho();
-    char one_length_str[2] = {0, 0};
-    char c{};
-    int currentPos = 1;
-
-    while((c = getch()) != 'a')
-    {
-        one_length_str[0] = c;
-        mvwprintw(input_text_win, 1, currentPos++, one_length_str);
-        wrefresh(input_text_win);
-    }
-
-    endwin();
-    return 0;
+    run_typing_trainer("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 }
