a88336741b70ce5ea7fb16bd04dec2268e190d02
FM <ge57sak@mytum.de>
2022-12-07 14:20:03 +0100

    Finished sprint 1

--- TypingTrainer.cpp
+++ TypingTrainer.cpp
@@ -1,40 +1,153 @@
-#include <iostream>
 #include <ncurses.h>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <chrono>
+#include <array>
+#include <memory>
 using namespace std;
 
-/*
-    This function return if a charachter may appear in the text (this excludes charachters like arrow, keyup, ...)
-*/
-bool is_valid_text_char(char c)
+// Ncurses defines values for the enter and backspace key that
+// do not work. Thus, we redefine their values to work on Linux machines.
+// (Tested on Ubuntu & Fedora Linux)
+#ifdef KEY_BACKSPACE
+    #undef KEY_BACKSPACE
+#endif
+#ifdef KEY_ENTER
+    #undef KEY_ENTER
+#endif
+#define KEY_BACKSPACE 127
+#define KEY_ENTER 10
+
+struct typing_stats
 {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') 
-    || c == '.' || c == ',' || c == ' ';
+    int chars_typed;
+    float elapsed_time_sec;
+    int num_of_mistakes;
+    float type_rate;
+};
+
+/*
+    The start menu will be shown to the user, right in the beginning.
+    It gives the user the ability to read through a short tutorial.
+*/
+void show_start_menu()
+{
+    initscr();
+
+    addstr("==========================================================\n");
+    addstr("|                                                        |\n");
+    addstr("|              Welcome to Typing Trainer!                |\n");
+    addstr("|               Press any key to continue.               |\n");
+    addstr("|                                                        |\n");
+    addstr("==========================================================\n");
+
+    getch();
+
+    clear();
+    addstr("==========================================================\n");
+    addstr("|                                                        |\n");
+    addstr("|         Do you want to read a short how-to?            |\n");
+    addstr("| Press 'y' for a tutorial, and 'n' to start right away. |\n");
+    addstr("|                                                        |\n");
+    addstr("==========================================================\n");
+
+    char input_char{};
+    while((input_char = getch()) != 'y' && input_char != 'n');
+
+    if(input_char == 'y')
+    {
+        clear();
+        addstr("==========================================================\n");
+        addstr("|                                                        |\n");
+        addstr("| Once you start the program, a text will be displayed   |\n");
+        addstr("| at the top of your screen. You will have to input that |\n");
+        addstr("| text on your keyboard. The text typing will end auto-  |\n");
+        addstr("| matically once you typed the whole text. You may also  |\n");
+        addstr("| terminate the trainer by hitting the enter key at any  |\n");
+        addstr("| point during the training. After having finished,      |\n");
+        addstr("| you can see statistics evaluating your performance. If |\n");
+        addstr("| you happen to make a typo, you will see a red marker.  |\n");
+        addstr("| To correct this typo, you may either use backspace or  |\n");
+        addstr("| simply keep typing. The wrong character will be rep-   |\n");
+        addstr("| laced by the correct character, as soon as you type    |\n");
+        addstr("| the correct character.                                 |\n");
+        addstr("|                                                        |\n");
+        addstr("| To start typing, press any character. The time tracker |\n");
+        addstr("| won't start until you type.                            |\n");
+        addstr("|                                                        |\n");
+        addstr("==========================================================\n");
+
+        getch();
+    }
+
+    clear();
+    endwin();
 }
 
 /*
-    read the sample text from the file and return's it
+    Ncurses reacts to any character input, i.e. also to character inputs that are no text
+    characters (e.g. backspace, arrow keys, enter, etc.). Such characters should obviously
+    not be treated as type-writer user input and thus, this function is designed to distinguish
+    between printable and non-printable characters.
 */
-string read_sample_text()
+bool is_valid_text_char(char c)
 {
-    ifstream is("beginner.txt");
-    string line;
+    const static array allowed_special_chars = {' ', '.', ',', '\'', 
+    '!', '?', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '\\', '/', 
+    '{', '[', ']', '}', '|', ':', ';', '<', '>', '~', '`'};
+
+    bool c_valid_text_char = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') 
+        || (c >= '0' && c <= '9');
+
+    for(int i = 0; i < allowed_special_chars.size() && !c_valid_text_char; i++)
+        c_valid_text_char = (c_valid_text_char || c == allowed_special_chars[i]);
+        
+    return c_valid_text_char;
+}
+
+/*
+    This function concatenates a file's content over several lines into
+    a single line and returns it as a string. It is being used to get the 
+    sample-training text from a file.
+*/
+string read_sample_text(string file_location)
+{
+    ifstream istr(file_location);
+    string current_line;
     string file_content="";
 
-    while (getline(is, line)) 
+    while (getline(istr, current_line)) 
     {
-        file_content += line;
+        file_content += current_line;
     }
    
     return file_content;
 }
 
 /*
-    This function runs the typing trainer.
+    This function saves typing statistics into a text file at
+    a specific location.
 */
-void run_typing_trainer(const string sample_text)
+void write_typing_stats(typing_stats stats, string file_location)
+{
+    ofstream ostr;
+    ostr.open(file_location);
+    if(ostr.is_open())
+    {
+        ostr << "Typing training statistics:\n\tCharacters typed: " << stats.chars_typed
+            << "\n\tMistakes made: " << stats.num_of_mistakes << "\n\tTime (sec.): "
+            << stats.elapsed_time_sec << "\n\tTyping speed (characters / min.): " << stats.type_rate << endl;
+        ostr.close();
+    }
+    else
+        cout << "Could not save typing statistics in a text file due to an error." << endl;
+}
+
+/*
+    This function runs the typing trainer and displays statistics after its termination.
+*/
+typing_stats run_typing_trainer(const string sample_text)
 {
     initscr();
 
@@ -56,6 +169,8 @@ void run_typing_trainer(const string sample_text)
     mvwprintw(input_text_win, 0, 1, "Your input");
     mvwprintw(statistic_info_win, 0, 1, "Statistics");
     mvwprintw(statistic_info_win, 1, 1, "Error Count: 0");
+    mvwprintw(statistic_info_win, 1, window_length - 23, "Press 'Enter' to stop");
+
     refresh();
     wrefresh(sample_text_win);
     wrefresh(input_text_win);
@@ -69,8 +184,9 @@ void run_typing_trainer(const string sample_text)
     // User's current typing position in text. E.g. position 4 for "Foo Bar" means 
     // that the user needs to input the character 'B' now
     int pos_in_sample_text = 0;
-    //count the numbers of the wrong charachters (red marked)
-    int typing_error=0;
+
+    // The numbers of wrong characters having been typed
+    int num_of_typing_errors = 0;
     
     // Number of characters in the sample text
     int len_of_sample_text = sample_text.size();
@@ -86,10 +202,13 @@ void run_typing_trainer(const string sample_text)
     // The character that has last been input
     char input_char;
 
-    //Calculate the time 
-    auto start_time=chrono::steady_clock::now();
+    // This will hold the time_point object of the time when the user typed the first letter
+    chrono::steady_clock::time_point start_time;
 
-    // endicates wether the user want to interrupt the programm early
+    // Whether the user started typing: Used for measuring start_time (see above).
+    bool user_started_typing = false;
+
+    // Indicates whether the user wants to interrupt and end the training
     bool user_interrupted_input = false;
 
 
@@ -99,8 +218,8 @@ void run_typing_trainer(const string sample_text)
         // Compute which part of the string is currently displayed in the window
         string current_text_segment = sample_text.substr(text_segment_start, displayable_text_len);
 
-        // Display the text to be shown
-        mvwprintw(sample_text_win, 1, 1, current_text_segment.c_str());
+        // Display the text to be shown (adding the trailing space is needed to avoid cosmetic issues)
+        mvwprintw(sample_text_win, 1, 1, (current_text_segment + " ").c_str());
         wrefresh(sample_text_win);
 
         // Refresh the shown text in the input window
@@ -111,6 +230,13 @@ void run_typing_trainer(const string sample_text)
         // If it was incorrect, repeat until it's correct
         while((input_char = getch()) != sample_text[pos_in_sample_text])
         {
+            // Measure the time when the user started typing
+            if(!user_started_typing)
+            {
+                user_started_typing = true;
+                start_time = chrono::steady_clock::now();
+            }
+
             // Input was incorrect: Display a red box around the wrongly input character
             if(is_valid_text_char(input_char))
             {
@@ -119,60 +245,99 @@ void run_typing_trainer(const string sample_text)
                 mvwaddch(input_text_win, 1, pos_in_sample_text - text_segment_start + 1, input_char);
                 wattroff(input_text_win,COLOR_PAIR(2));
                 wrefresh(input_text_win);
-                typing_error++;
-                mvwprintw(statistic_info_win, 1, 1, ("Error Count: " + to_string(typing_error)).c_str());
+                num_of_typing_errors++;
+                mvwprintw(statistic_info_win, 1, 1, ("Error Count: " + to_string(num_of_typing_errors)).c_str());
                 wrefresh(statistic_info_win);
             }
-            else if(input_char=='\n')
+            else if(input_char == KEY_ENTER)
             {
                 user_interrupted_input = true;
                 break;
             }
-            else if(input_char=='!')
+            else if(input_char == KEY_BACKSPACE)
             {
                 mvwaddch(input_text_win, 1, pos_in_sample_text - text_segment_start + 1, ' '); 
                 wmove(input_text_win, 1, pos_in_sample_text - text_segment_start + 1);
                 wrefresh(input_text_win);
             }
+            else
+            {
+                /*
+                This is a workaround for an odd peculiarity of ncurses:
+                Some special inputs (e.g. arrow keys, escape key, etc) are
+                retrieved by calling getch() twice or thrice. The arrow key
+                is thus not passed to getch() by one single character but by
+                several characters that all need to be read. The issue is that
+                we do not want to consider special inputs. Thus, when we encounter
+                unwanted characters, we flush the whole character input queue 
+                in order to not fetch "weird" character values in the next getch()
+                call (that actually still belong to e.g. an input arrow key)
+                */
+                flushinp();
+            }
             
         }
+
+        // Measure the time when the user started typing
+        if(!user_started_typing)
+        {
+            user_started_typing = true;
+            start_time = chrono::steady_clock::now();
+        }
+
         // This is a cosmetic fix that is needed
         mvwaddch(input_text_win, 1, pos_in_sample_text - text_segment_start + 1, ' '); 
 
-        // When we finally input the correct char, we mve on to the next
-        pos_in_sample_text++;
+        // When we finally input the correct char, we move on to the next
+        if(!user_interrupted_input)
+            pos_in_sample_text++;
 
         // In the beginning, the current input position will move from left to right.
         // At half of the window size, we stop moving the input position and move
         // the whole text instead
         if(pos_in_sample_text > displayable_text_len / 2)
             text_segment_start++;
-        //check if the last charachter has been input and refresh the window
+
+        // After the last character of the text has been entered, we need to 
+        // refresh the window again (this typically happens in the loop above)
+        // but since we are at the end of the text, the loop won't execute anymore
         if(pos_in_sample_text == len_of_sample_text)
         {
             // Refresh the shown text in the input window
-            mvwprintw(input_text_win, 1, 1, (current_text_segment.substr(0, pos_in_sample_text - text_segment_start)).c_str());
+            mvwprintw(input_text_win, 1, 1, (current_text_segment.substr(0, pos_in_sample_text - text_segment_start + 1)).c_str());
             wrefresh(input_text_win);
         }
     }
 
     //calculate the user time depending on start and end time
-    auto end_time2=chrono::steady_clock::now();
-    auto elapsed_time=int(chrono::duration_cast<chrono::seconds>(end_time2-start_time).count());
-    mvwprintw(statistic_info_win, 1, 25, ("Your time is: "+to_string(elapsed_time) + " seconds").c_str());
-    mvwprintw(statistic_info_win, 1, 65, ("Press Enter to Exit:"));
+    auto end_time = chrono::steady_clock::now();
+    auto elapsed_time = int(chrono::duration_cast<chrono::seconds>(end_time - start_time).count());
+    float chars_per_minute;
+    if(elapsed_time > 0)
+        chars_per_minute = pos_in_sample_text / elapsed_time * 60.;
+    else
+        chars_per_minute = 0;
+    mvwprintw(statistic_info_win, 1, 20, ("Elapsed time: " + to_string(elapsed_time) + "sec.").c_str());
+    mvwprintw(statistic_info_win, 1, 44, ("Characters / min.: " + to_string(chars_per_minute)).c_str());
+    mvwprintw(statistic_info_win, 1, window_length - 23, "Press 'Enter' to exit");
     wrefresh(statistic_info_win);
     while(getch() != '\n');
 
     endwin();
 
+    typing_stats training_stats;
+    training_stats.chars_typed = pos_in_sample_text;
+    training_stats.elapsed_time_sec = elapsed_time;
+    training_stats.type_rate = chars_per_minute;
+    training_stats.num_of_mistakes = num_of_typing_errors;
 
-
-
+    return training_stats;
 }
 
 
 int main()
 {
-    run_typing_trainer(read_sample_text());
+    show_start_menu();
+    typing_stats training_stats = run_typing_trainer(read_sample_text("beginner.txt"));
+    write_typing_stats(training_stats, "statistics.txt");
 }
